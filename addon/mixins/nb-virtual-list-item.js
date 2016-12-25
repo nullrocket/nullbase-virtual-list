7d1be18531be7e193fe58117e4e6e470aff85839
nullrocket <doug.hill@passpack.com>
2016-12-24 20:20:07 -0800

    Initial Commit

--- addon/mixins/nb-virtual-list-item.js
+++ addon/mixins/nb-virtual-list-item.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
-export default Ember.Mixin.create({
+import SpreadMixin from 'ember-spread'
+export default Ember.Mixin.create(SpreadMixin,{
   parentER : null,
 
   didInsertElement: function () {
@@ -7,14 +8,14 @@ export default Ember.Mixin.create({
 
     this.get('parentView.childComponents').pushObject(this);
 
-    this._super();
+    this._super(...arguments);
   },
   willDestroyElement: function () {
     if (this.get('parentER')) {
 
       this.get('parentER').removeObject(this);
     }
-    this._super();
+    this._super(...arguments);
   }
 
 });
