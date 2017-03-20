5fbf7be27655122648ae1d097890b2ea60a931fc
nullrocket <doug.hill@passpack.com>
2017-03-20 02:17:54 -0700

    Fix for scrollTop when using native scroll.

--- addon/mixins/nb-virtual-list.js
+++ addon/mixins/nb-virtual-list.js
@@ -159,7 +159,7 @@ export default Ember.Mixin.create({
               var itemCount = self.get('items') ? self.get('items').length : 0;
               var contentHeight = $contentElement.height();
               var viewPortHeight = self.get('height');
-              var firstInView = ((Math.floor(scrollTop / itemHeight) - 100) > 0) ? (Math.floor(scrollTop / itemHeight) - 100) : 0;
+              var firstInView = ((Math.floor(scrollTop / itemHeight) - 10) > 0) ? (Math.floor(scrollTop / itemHeight) - 10) : 0;
               ren.updates = [];
               var childViewLength = self.get('childViewsX').length;
               var i;
@@ -394,7 +394,7 @@ export default Ember.Mixin.create({
         }
         self.get('childViewsX').pushObjects(objects);
 
-        var firstInView = ((Math.floor(scrollTop / itemHeight) - 4) > 0) ? (Math.floor(scrollTop / itemHeight) - 4) : 0;
+        var firstInView = ((Math.floor(scrollTop / itemHeight) - 100) > 0) ? (Math.floor(scrollTop / itemHeight) - 100) : 0;
 
         var updates = [];
         var childViewLength = self.get('childViewsX').length;
